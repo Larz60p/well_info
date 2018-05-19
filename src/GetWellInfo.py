08829d15148d4bfe0692c922c20163c7ddd06512
Larz60+ <Larz60p@users.noreply.github.com>
2018-05-19 06:27:33 -0400

    Create GetWellInfo.py
    Web scraper for completion reports

---
+++ src/GetWellInfo.py
@@ -0,0 +1,12 @@
+import WellPaths
+
+
+class GetWellInfo.py:
+    def __init__(self):
+        self.wpaths = WellPaths.WellPaths()
+
+def main():
+    GetWellInfo()
+
+if __name__ == '__main__':
+    main()
