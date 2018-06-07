67fb43ddf91118c12114866a6efdbe020ae71407
Larz60+ <Larz60p@users.noreply.github.com>
2018-06-07 15:49:28 -0400

    Update __init__.py
    New edition

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
-well_info/
-    __init__.py
+WellInfo/
     src/
         __init__.py
+        CheckInternet.py
+        Codes.py
+        FindInfoUrls.py
         WellPaths.py
-        GetWellInfo.py
-        MakeApiTxt.py
+
